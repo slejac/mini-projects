03bc0554778439579c0c98704d4c4715c775fe97
Jack Sleeman <slejac20@gmail.com>
2021-01-09 18:49:47 -0500

    Updated Repo, Removal of Incomplete Project

--- C++/tic_tac_toe/tic_tac_toe.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <vector>
-using std::vector;
-
-void generate_board(const vector<vector<int>> &);
-bool incomplete(const vector<vector<int>> &);
-void easy_mode(vector<vector<int>> &);
-void hard_mode(vector<vector<int>> &);
