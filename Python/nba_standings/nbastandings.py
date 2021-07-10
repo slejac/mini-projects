20ba4c1eeedb909cdebd962e1abe185190343665
Jack Sleeman <slejac20@gmail.com>
2021-07-10 17:04:51 -0400

    NBA Standings Displayer Checkpoint

--- Python/nba_standings/nbastandings.py
+++ Python/nba_standings/nbastandings.py
@@ -1,8 +1,10 @@
+# Add conference functionality (teaminfocommon endpoint)
+# Resolve possible issue with API Read Timeouts?
+
 from nba_api.stats.static import teams
 from nba_api.stats.endpoints import teamyearbyyearstats
 import numpy as np
 import pandas as pd
-from termcolor import colored
 teams = teams.get_teams()
 ids = []
 name = []
@@ -15,17 +17,4 @@ for team in arr:
     data = pd.DataFrame(teamyearbyyearstats.TeamYearByYearStats(team_id=team[0]).get_data_frames()[0])
     pct.append(data["WIN_PCT"].iloc[-1])
 arr = np.column_stack((arr, pct))
-#arr.sort(key=lambda x: x[2])
-odds = ["14%", "14%", "14%", "11.5%", "11.5%", "9%", "7.5%", "4.5%", "4.5%", "4.5%", "1.8%", "1.7%", "1%", "0.5%", "0%","0%","0%","0%","0%","0%","0%","0%","0%","0%","0%","0%","0%","0%","0%","0%"]
-arr = np.column_stack((arr, odds))
-counter = 1
-print("Tankathon: NBA 1st Pick Odds Tracker")
-for teams in arr:
-    print(counter, end='')
-    if teams[2] > .5:
-        print(colored(team[1], 'green'), teams[3])
-    elif team[2] == .5:
-        print(colored(team[1], 'yellow'), teams[3])
-    else:
-        print(colored(team[1], 'red'), teams[3])
-    counter += 1
+print(arr)
